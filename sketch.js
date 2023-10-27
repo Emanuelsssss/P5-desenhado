f4424e5294510b5b2653a5d846939c76d894d39c
Emanuelsssss <145786384+Emanuelsssss@users.noreply.github.com>
2023-10-27 12:13:44 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  
+    createCanvas(600, 600);
+    
+     background ("yellow");
+    
+  }
+  
+  function draw() {
+    
+     stroke ("blue");
+    
+     fill("red");
+    
+    if (mouseIsPressed){
+      
+      rect(mouseX, mouseY ,50, 65);
+    }
+  }
